27a202aaf7fa32a04246bc3bb1bf8131aaebb965
Andrew Cho and lawton b <cho-andrew@hotmail.com and lawton.a.browning@gmail.com>
2016-02-16 16:51:52 -0800

    end the day...will figure out problem with intersect method later.

--- Objects/AnagramObjects.cs
+++ Objects/AnagramObjects.cs
@@ -21,20 +21,26 @@ namespace Anagrams
     {
       char[] _inputInfo = _userInput1.ToCharArray();
       char[]  _inputInfo2 = _userInput2.ToCharArray();
+      // IEnumerable <char[]> _inputInfoCompared = _inputInfo.Intersect(_inputInfo2);
       Array.Sort(_inputInfo);
       Array.Sort(_inputInfo2);
 
-      if (_inputInfo == _inputInfo2)
+      if (_inputInfo.SequenceEqual(_inputInfo2))
       {
-        return _userInput2;
+        return _userInput2 + " is an anagram of " + _userInput1;
       }
-      else if (_inputInfo.SequenceEqual(_inputInfo2))
-      {
-        return _userInput2;
-      }
+
+      // else if (_inputInfoCompared.SequenceEqual(_inputInfo))
+      // {
+        // foreach (char[] _inputInfo in _inputInfo2)
+        // {
+        //   Console.WriteLine("THIS IS WORKING");
+        // }
+        // return "True";
+      // }
       else
       {
-        return _userInput1;
+        return _userInput1 + " is NOT an anagram of " + _userInput2;
       }
     }
   }
--- Objects/AnagramTest.cs
+++ Objects/AnagramTest.cs
@@ -9,7 +9,7 @@ namespace AnagramTest
   public class AnagramTester
   {
     [Fact]
-    public void IsWord_forWordEqualingSameWord_true()
+    public void IsWord_forWordEqualingSameWord_input1()
     {
       string input1 = "beard";
       string input2 = "beard";
@@ -18,7 +18,7 @@ namespace AnagramTest
     }
 
     [Fact]
-    public void IsWord_forWordNotEqualingSameWord_false()
+    public void IsWord_forWordNotEqualingSameWord_input1()
     {
       string input1 = "frog";
       string input2 = "superman";
@@ -27,7 +27,7 @@ namespace AnagramTest
     }
 
     [Fact]
-    public void IsWord_ForWordIsAnagramOfSameWord_true()
+    public void IsWord_ForWordIsAnagramOfSameWord_input2()
     {
       string input1 = "and";
       string input2 = "dna";
@@ -35,6 +35,16 @@ namespace AnagramTest
       Anagram newAnagram = new Anagram(input1,input2);
 
       Assert.Equal(input2, newAnagram.CompareInputs());
+    }
+    [Fact]
+    public void IsWord_ForWordIsPartOfSameWord_input1()
+    {
+      string input1 = "hat";
+      string input2 = "that";
+
+      Anagram newAnagram = new Anagram(input1,input2);
+
+      Assert.Equal(input1, newAnagram.CompareInputs());
     }
   }
 }
